9a019d882c195dacbcc4fd58e4e0408e2a702295
Gustavo <gustavociscotto@gmail.com>
2025-07-08 13:33:12 -0300

    feat : pastas

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from 'dotenv';
 import cors from "cors"
 
-import { route } from "./routes/route.js";
+import { route } from "./src/routes/route.js";
 
 
 
